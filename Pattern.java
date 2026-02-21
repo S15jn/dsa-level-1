ce70a7f01d87517572fb70693a527b34bd4f1c8a
S15jn <agjahanvi@gmail.com>
2026-02-22 02:26:04 +0530

    day 8

--- Pattern.java
+++ Pattern.java
@@ -179,7 +179,18 @@ public class Pattern {
         System.out.println();
     }
     }
+    public static void Pattern20(int n){
+        for(int i=1;i<=n;i++){
+            for(int k=1;k<=(n-i);k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        Pattern19(5);
+        Pattern20(5);
     }
 }
